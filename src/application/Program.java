90325520584c8e67085fbc032deff2813b92cc35
vaz-27 <martriz4ever@gmail.com>
2020-10-01 14:36:04 -0300

    implementacao deleteById

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -13,6 +14,8 @@ public class Program {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
+		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println("=== TEST 1: seller findId ===");		
@@ -46,5 +49,14 @@ public class Program {
 		seller.setEmail("marta@gmail.com");
 		sellerDao.update(seller);
 		System.out.println("Update completed");
+		
+		System.out.println("\n\n=== TEST 6: seller delete ===");
+		System.out.print("Enter id for delete teste: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed");
+		
+		
+		sc.close();
 	}		
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -88,14 +88,30 @@ public class SellerDaoJDBC implements SellerDao{
 		}
 		finally {
 			DB.closeStatement(st);			
-		}		
-		
+		}			
 	}
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
-		
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");	
+			st.setInt(1, id);
+			
+			int rowsAffected = st.executeUpdate();
+					
+			if(rowsAffected == 0) {
+				throw new DbException("Invalid id");
+			}
+					
+					
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}		
 	}
 
 	@Override
