66404305d0c496b96089afd06e7df58adcf44e40
vaz-27 <martriz4ever@gmail.com>
2020-10-09 18:56:04 -0300

    Pacote de testes criado

---
+++ src/model/dao/impl/test/DepartmentDaoJDBCTest.java
@@ -0,0 +1,20 @@
+/*package model.dao.impl.test;
+
+import org.junit.Test;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class DepartmentDaoJDBCTest {
+	
+	DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+	
+	@Test
+	public void insertValidoTest() {
+		Department dep = new Department(null, "Music");
+		departmentDao.insert(dep);
+	}
+
+}
+*/
