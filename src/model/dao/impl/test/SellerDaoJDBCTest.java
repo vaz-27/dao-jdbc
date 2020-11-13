b06eb4af28824ffcb4d5adb1629133497118ec62
vaz-27 <martriz4ever@gmail.com>
2020-11-13 16:05:46 -0300

    iniciando testes em seller

--- src/model/dao/impl/test/SellerDaoJDBCTest.java
+++ src/model/dao/impl/test/SellerDaoJDBCTest.java
@@ -3,6 +3,7 @@ package model.dao.impl.test;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
@@ -19,5 +20,17 @@ public class SellerDaoJDBCTest {
 	@Before
 	public void cenario() {
 		initMocks(this);
+	}
+	
+	@Test(expected = db.DbException.class)
+	public void deletandoById() {
+		//cenario
+		seller.setId(100);
+		
+		//acao
+		sellerdao.deleteById(100);
+		
+		//verificacao
+		sellerdao.findById(100);
 	}
 }
